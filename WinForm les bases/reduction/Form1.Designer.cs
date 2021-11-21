0725bcaeefb2fd94fdebcf693038b7555c1d2a30
OkonoWil <okonowilfried@gmail.com>
2021-11-21 05:40:02 +0100

    ajout TP réduction

---
+++ WinForm les bases/reduction/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace reduction
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_montant = new System.Windows.Forms.TextBox();
+            this.txt_taux = new System.Windows.Forms.TextBox();
+            this.txt_total = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_err = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(270, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Saisir le montant d\'achat en Franc CFA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(41, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 19);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "La somme à payer est";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(41, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(176, 19);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Le taux de réduction est ";
+            // 
+            // txt_montant
+            // 
+            this.txt_montant.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.txt_montant.Location = new System.Drawing.Point(361, 35);
+            this.txt_montant.Name = "txt_montant";
+            this.txt_montant.Size = new System.Drawing.Size(227, 26);
+            this.txt_montant.TabIndex = 1;
+            this.txt_montant.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txt_taux
+            // 
+            this.txt_taux.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.txt_taux.Location = new System.Drawing.Point(361, 89);
+            this.txt_taux.Name = "txt_taux";
+            this.txt_taux.ReadOnly = true;
+            this.txt_taux.Size = new System.Drawing.Size(227, 26);
+            this.txt_taux.TabIndex = 1;
+            this.txt_taux.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txt_total
+            // 
+            this.txt_total.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.txt_total.Location = new System.Drawing.Point(361, 137);
+            this.txt_total.Name = "txt_total";
+            this.txt_total.ReadOnly = true;
+            this.txt_total.Size = new System.Drawing.Size(227, 26);
+            this.txt_total.TabIndex = 1;
+            this.txt_total.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Location = new System.Drawing.Point(396, 215);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 45);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Calculer";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_err
+            // 
+            this.txt_err.AutoSize = true;
+            this.txt_err.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_err.ForeColor = System.Drawing.Color.Red;
+            this.txt_err.Location = new System.Drawing.Point(48, 277);
+            this.txt_err.Name = "txt_err";
+            this.txt_err.Size = new System.Drawing.Size(0, 19);
+            this.txt_err.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 335);
+            this.Controls.Add(this.txt_err);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_total);
+            this.Controls.Add(this.txt_taux);
+            this.Controls.Add(this.txt_montant);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Réduction";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_montant;
+        private System.Windows.Forms.TextBox txt_taux;
+        private System.Windows.Forms.TextBox txt_total;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label txt_err;
+    }
+}
+
---
+++ WinForm les bases/reduction/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace reduction
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            decimal montant;
+            if(decimal.TryParse(txt_montant.Text, out montant) && montant >= 0)
+            {
+                txt_err.Text = "";
+
+                //Calcul du taux de réduction
+                int taux;
+                if (montant <= 1000)
+                    taux = 0;
+                else if (montant <= 3000)
+                    taux = 5;
+                else if (montant <= 4000)
+                    taux = 10;
+                else if (montant <= 5000)
+                    taux = 15;
+                else
+                    taux = 20;
+                txt_taux.Text = taux.ToString() + "%";
+
+                //Calcul du montant total à payer
+                decimal total = montant - (montant * taux) / 100;
+                txt_total.Text = total.ToString();
+            }
+            else
+            {
+                txt_err.Text = "Erreur : saisir un montant valide";
+                txt_taux.Text = "";
+                txt_total.Text = "";
+            }
+        }
+    }
+}
